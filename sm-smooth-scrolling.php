b9b02fb6f2390f3d248e05b08d8af03d53cba1f2
Sulaiman Misri <msulaimanmisri@gmail.com>
2023-08-22 13:18:20 +0800

    plugin initialize

---
+++ sm-smooth-scrolling.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Plugin Name: SM Smooth Scrolling
+ * Plugin URI: https://github.com/msulaimanmisri/sm-smooth-scrolling
+ * Description: Make your WordPress wheel effect smooth!
+ * Version: 1.0
+ * Author: Sulaiman Misri
+ * Author URI: https://github.com/msulaimanmisri
+ * Text Domain: sm-smooth-scrolling
+ * Domain Path: /languages
+ * Tested up to: 6.1.1
+ * License: GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
